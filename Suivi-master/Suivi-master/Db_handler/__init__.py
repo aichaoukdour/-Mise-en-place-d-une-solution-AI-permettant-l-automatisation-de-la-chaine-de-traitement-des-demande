a459f098500452025acb26c4a81a041d03edf9d8
aichaoukdour <147880095+aichaoukdour@users.noreply.github.com>
2025-05-17 14:37:04 +0200

    Remove mistakenly pushed nested folder

--- Suivi-master/Suivi-master/Db_handler/__init__.py
+++
@@ -1,3 +0,0 @@
-# Db_handler/__init__.py
-from .Database import EmailDatabase
-EmailDatabase_instance = EmailDatabase()
--- Suivi-master/Suivi-master/EmailReporting/admin.py
+++
@@ -1,6 +0,0 @@
-from email.message import EmailMessage
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import *
